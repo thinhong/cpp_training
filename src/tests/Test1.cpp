b79fed6bc56d09b17f34c9af69a3921e70a7819f
thinh <ongphucthinh@gmail.com>
2021-02-25 12:10:26 +0700

    test: unit testing checkCycle with googletest

--- src/tests/Test1.cpp
+++ src/tests/Test1.cpp
@@ -15,6 +15,52 @@
 #include "../Model.h"
 #include "../Model.cpp"
 
+TEST(ModelTest, checkCycle) {
+    Compartment::daysFollowUp = 200;
+    Distribution::errorTolerance = 0.01;
+    auto bernoulli = std::make_shared<BernoulliDistribution>(std::make_shared<double>(0.1));
+    auto bernoulli_removed = std::make_shared<BernoulliDistribution>(std::make_shared<double>(0.0));
+    auto gamma_7d = std::make_shared<DiscreteGammaDistribution>(1, 1);
+    auto gamma_13d = std::make_shared<DiscreteGammaDistribution>(1, 4);
+    auto weil_16d = std::make_shared<DiscreteWeibullDistribution>(5, 1.5);
+    auto weil_9d = std::make_shared<DiscreteWeibullDistribution>(4, 3);
+
+    auto S = std::make_shared<Compartment>("S", 3000000, bernoulli);
+    auto E = std::make_shared<Compartment>("E", 0, weil_9d);
+    auto A = std::make_shared<Compartment>("A", 0, gamma_7d);
+    auto A_r = std::make_shared<Compartment>("A_r", 0, weil_16d);
+    auto I = std::make_shared<Compartment>("I", 1, gamma_13d);
+    auto H_h = std::make_shared<Compartment>("H_h", 0, gamma_7d);
+    auto H_c = std::make_shared<Compartment>("H_c", 0, weil_9d);
+    auto H_d = std::make_shared<Compartment>("H_d", 0, gamma_7d);
+    auto C_c = std::make_shared<Compartment>("C_c", 0, weil_16d);
+    auto C_d = std::make_shared<Compartment>("C_d", 0, gamma_13d);
+    auto D = std::make_shared<Compartment>("D", 0, bernoulli_removed);
+    auto R = std::make_shared<Compartment>("R", 0, bernoulli_removed);
+
+    Model myModel;
+
+    // Add and also connect all compartments into myModel
+    myModel.addCompsAndConnect(S, E);
+    myModel.addCompsAndConnect(E, A);
+    myModel.addCompsAndConnect(A, A_r, 0.35);
+    myModel.addCompsAndConnect(A, I, 0.65);
+    myModel.addCompsAndConnect(A_r, R);
+    myModel.addCompsAndConnect(I, H_h, 0.7);
+    myModel.addCompsAndConnect(I, H_c, 0.2);
+    myModel.addCompsAndConnect(I, H_d, 0.1);
+    myModel.addCompsAndConnect(H_h, R);
+    myModel.addCompsAndConnect(H_c, C_c);
+    myModel.addCompsAndConnect(C_c, R);
+    myModel.addCompsAndConnect(H_d, C_d);
+    myModel.addCompsAndConnect(C_d, D);
+    // Test with a isCycle: C_c going back to S
+    myModel.addCompsAndConnect(C_c, S);
+
+    myModel.checkCycle();
+    EXPECT_TRUE(myModel.getCycle());
+}
+
 TEST(ModelTest, sortComp) {
     Compartment::daysFollowUp = 200;
     Distribution::errorTolerance = 0.01;
@@ -99,10 +145,10 @@ TEST(ModelTest, sortComp) {
         indexFlow4.push_back(myModel.getIndex(i));
     }
 
-    ASSERT_TRUE(std::is_sorted(indexFlow1.begin(), indexFlow1.end()));
-    ASSERT_TRUE(std::is_sorted(indexFlow2.begin(), indexFlow2.end()));
-    ASSERT_TRUE(std::is_sorted(indexFlow3.begin(), indexFlow3.end()));
-    ASSERT_TRUE(std::is_sorted(indexFlow4.begin(), indexFlow4.end()));
+    EXPECT_TRUE(std::is_sorted(indexFlow1.begin(), indexFlow1.end()));
+    EXPECT_TRUE(std::is_sorted(indexFlow2.begin(), indexFlow2.end()));
+    EXPECT_TRUE(std::is_sorted(indexFlow3.begin(), indexFlow3.end()));
+    EXPECT_TRUE(std::is_sorted(indexFlow4.begin(), indexFlow4.end()));
 }
 
 int main(int argc, char **argv) {
