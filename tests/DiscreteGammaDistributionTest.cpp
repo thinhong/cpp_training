92449cd29682f2c6619dcb38d2dfec93116f061e
thinh <ongphucthinh@gmail.com>
2021-03-03 11:16:04 +0700

    test: unit testing for update model

--- tests/DiscreteGammaDistributionTest.cpp
+++ tests/DiscreteGammaDistributionTest.cpp
@@ -29,16 +29,18 @@ TEST(ModelTest, test) {
     const double populationSize = 1000000;
     const double transRate = 2.0;
     auto forceInfection = std::make_shared<double>();
-    std::vector<double> cumProb = {0.0, 0.8, 1};
+    std::vector<double> cumProb = {0.0, 0.3, 1};
     auto S = make_shared<Compartment>("S", 10000, make_shared<BernoulliDistribution>(forceInfection));
-    auto E = make_shared<Compartment>("E", 500, make_shared<DiscreteGammaDistribution>(cumProb, 3));
+    auto I = make_shared<Compartment>("I", 500, make_shared<DiscreteGammaDistribution>(cumProb, 3));
     auto R = make_shared<Compartment>("R", 0, make_shared<BernoulliDistribution>(make_shared<double>(0.0)));
+    auto D = make_shared<Compartment>("D", 0, make_shared<BernoulliDistribution>(make_shared<double>(0.0)));
 
     Model myModel;
-    myModel.addCompsAndConnect(S, E, 1);
-    myModel.addCompsAndConnect(E, R, 1);
+    myModel.addCompsAndConnect(S, I, 1);
+    myModel.addCompsAndConnect(I, R, 0.9);
+    myModel.addCompsAndConnect(I, D, 0.1);
 
-    std::vector<std::string> infectiousComps {"E"};
+    std::vector<std::string> infectiousComps {"I"};
 
     myModel.sortComps();
     for (size_t i {1}; i < Compartment::daysFollowUp; i++) {
@@ -54,8 +56,37 @@ TEST(ModelTest, test) {
         myModel.update(i);
     }
     EXPECT_EQ(S->getName(), myModel.getComps()[0]->getName());
-    EXPECT_EQ(9990, myModel.getComps()[0]->getTotal()[1]);
-    EXPECT_EQ(510, myModel.getComps()[1]->getTotal()[1]);
+
+    size_t iter;
+    double total;
+    // Iteration 1
+    iter = 1;
+    EXPECT_EQ(9990, myModel.getComps()[0]->getTotal()[iter]);
+    EXPECT_EQ(510, myModel.getComps()[1]->getTotal()[iter]);
+    // Iteration 2
+    iter = 2;
+    EXPECT_NEAR(9979.81, myModel.getComps()[0]->getTotal()[iter], 0.01);
+    EXPECT_NEAR(370.19, myModel.getComps()[1]->getTotal()[iter], 0.01);
+    EXPECT_EQ(15, myModel.getComps()[2]->getTotal()[iter]);
+    EXPECT_EQ(135, myModel.getComps()[3]->getTotal()[iter]);
+    // Assure that total value remain constant
+    total = 0;
+    for (auto& comp: myModel.getComps()) {
+        total += comp->getTotal()[iter];
+    }
+    EXPECT_NEAR(10500, total, 0.01);
+    // Iteration 3
+    iter = 3;
+    EXPECT_NEAR(9972.421, myModel.getComps()[0]->getTotal()[iter], 0.01);
+    EXPECT_NEAR(24.5786, myModel.getComps()[1]->getTotal()[iter], 0.01);
+    EXPECT_NEAR(50.3, myModel.getComps()[2]->getTotal()[iter], 0.01);
+    EXPECT_NEAR(452.7, myModel.getComps()[3]->getTotal()[iter], 0.01);
+    // Assure that total value remain constant
+    total = 0;
+    for (auto& comp: myModel.getComps()) {
+        total += comp->getTotal()[iter];
+    }
+    EXPECT_NEAR(10500, total, 0.01);
 }
 
 int main(int argc, char **argv) {
